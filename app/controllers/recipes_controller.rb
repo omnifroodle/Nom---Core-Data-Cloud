c1ce890a8b9f06667ff3981a644ccdfaee70ab5c
Matt Overstreet <matt.overstreet@gmail.com>
2010-02-27 15:24:37 -0500

    addedd plist to default recipe view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,6 +7,7 @@ class RecipesController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @recipes }
+      format.plist { render :layout => false}
     end
   end
 
