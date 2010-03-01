c87644f9984fa7a635fbf3ef62cf27388c1dc02e
Matt Overstreet <matt.overstreet@gmail.com>
2010-02-28 23:18:51 -0500

    typo corrected

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
-class Recipe < ActiveRecord::Base0
+class Recipe < ActiveRecord::Base
   named_scope :from, lambda { |lastid|
     unless lastid.blank? or lastid.to_i == 0 
       { :conditions => ['updated_at >= ?', Time.at(lastid.to_i)] }
