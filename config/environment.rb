7b2112a0e60107827b42a1efcd0b727210e5255f
Bob Larrick <larrick@gmail.com>
2010-03-03 13:34:52 -0500

    added plist gem to environment and unpacked it

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
 
   config.gem 'formtastic'
   config.gem 'validation_reflection'
+  config.gem 'plist'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
